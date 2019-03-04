c57690b37bafef8a3da71f31c6f8e855d7dace44
bwritsch <b.writsch@yahoo.de>
2019-03-04 19:19:44 +0100

    IRepository Erweitern

--- RepositoryMvc/RepositoryMvc/Contract/IAuthorRepository.cs
+++ RepositoryMvc/RepositoryMvc/Contract/IAuthorRepository.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace RepositoryMvc.Contract
 {
-    public interface IAuthorRepository
+    public interface IAuthorRepository : IRepository<Author>
     {
 
         List<Author> GeAuthorList();
